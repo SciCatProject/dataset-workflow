c5dee89af44cfc5f3ff9133a8c41c706cdddd185
Henrik Johansson <henrik.johansson712@gmail.com>
2019-06-04 16:50:35 +0200

    Started working on action mantid-reduce-dataset that combines actions mantid-reduce and reduce-dataset

--- create-actions.sh
+++ create-actions.sh
@@ -1,3 +1,5 @@
 wsk -i action update reduce-dataset ./src/actions/reduce_dataset.py -P config.local.json --docker dacat/openwhisk-python2action-mantid:latest
 
 wsk -i action update mantid-reduce ./src/actions/mantid_reduce.py -m 1024 --docker dacat/openwhisk-python2action-mantid:latest
+
+wsk -i action update mantid-reduce-dataset ./src/actions/mantid_reduce_dataset.py -P config.local.json -m 1024 --docker dacat/openwhisk-python2action-mantid:latest
---
+++ src/actions/mantid_reduce_dataset.py
@@ -0,0 +1,161 @@
+from __future__ import (absolute_import, division, print_function)
+import datetime
+import random
+import string
+import subprocess
+import requests
+from requests.exceptions import Timeout
+from mantid.simpleapi import *
+from ISISCommandInterface import *
+
+
+def main(input_data):
+    if 'datasetPid' in input_data:
+        catamel = Catamel(input_data)
+        sans2d = SANS2DLimitEventsTime(input_data)
+        utils = Utils()
+
+        # dataset_name = input_data['dataset']['datasetName']
+        dataset_pid = input_data['datasetPid']
+        access_token = catamel.login()
+
+        if len(access_token) == 64:
+            input_dataset = catamel.fetch_dataset_from_pid(access_token, dataset_pid)
+            reduce_data = sans2d.run()
+            derived_dataset = utils.new_derived_dataset(input_dataset, reduce_data)
+            post_response = catamel.post_derived_dataset(access_token, derived_dataset)
+            message = "Success: Dataset reduction complete."
+            # return {"input_dataset": input_dataset, "reduce_data": reduce_data}
+            return {"derivedDataset": post_response, "message": message}
+        else:
+            return {"message": access_token}
+
+    else:
+        dataset_name = "Unknown"
+        message = "Error: Input did not reach Action 'reduce-dataset'."
+        return {"datasetName": dataset_name, "message": message}
+
+
+class SANS2DLimitEventsTime(object):
+
+    def __init__(self, input_data):
+        self.input_data = input_data
+
+    def run(self):
+        SANS2D()
+        MaskFile('/data/MaskSANS2DReductionGUI_LimitEventsTime.txt')
+        AssignSample('/data/SANS2D00022048.nxs')
+        return WavRangeReduction()
+
+
+class Catamel:
+
+    def __init__(self, input_data):
+        self.host = input_data['api']['host']
+        self.port = input_data['api']['port']
+        self.login_details = input_data['login']
+
+    def login(self):
+        try:
+            login_response = requests.post(
+                'http://' + self.host + ':' + self.port + '/api/v3/Users/Login',
+                json=self.login_details,
+                timeout=(5, 10)
+            )
+        except Timeout:
+            return "Error: Login request timed out."
+        else:
+            return login_response.json()['id']
+
+    def post_derived_dataset(self, access_token, derived_dataset):
+        try:
+            post_response = requests.post(
+                'http://' + self.host + ':' + self.port + '/api/v3/DerivedDatasets?access_token=' + access_token,
+                json=derived_dataset,
+                timeout=(5, 10)
+            )
+        except Timeout:
+            return "Error: Post request timed out."
+        else:
+            return post_response.json()
+
+    def fetch_dataset_from_pid(self, access_token, dataset_pid):
+        formatted_pid = dataset_pid.replace("/", "%2F")
+        try:
+            fetch_response = requests.get(
+                'http://' + self.host + ':' + self.port + '/api/v3/Datasets/' + formatted_pid + '?access_token=' + access_token,
+                timeout=(5, 10)
+            )
+        except Timeout:
+            return "Error: Get request timed out."
+        else:
+            return fetch_response.json()
+
+
+class Utils:
+
+    def random_pid(self, letters=string.ascii_uppercase, digits=string.digits):
+        return ''.join(random.choice(letters) for _ in range(3)) + ''.join(random.choice(digits) for _ in range(3))
+
+    def new_derived_dataset(self, input_dataset, reduce_data):
+        timestamp = datetime.datetime.now().isoformat()
+
+        return {
+            "investigator": input_dataset['owner'],
+            "inputDatasets": [
+                input_dataset['pid']
+            ],
+            "usedSoftware": [
+                "mantidpython " + subprocess.check_output(["mantidpython", "--version"]).rstrip()
+            ],
+            "jobParameters": {},
+            "jobLogData": reduce_data,
+            "scientificMetadata": {},
+            "owner": input_dataset['owner'],
+            "ownerEmail": input_dataset['ownerEmail'],
+            "orcidOfOwner": input_dataset['orcidOfOwner'],
+            "contactEmail": input_dataset['contactEmail'],
+            "sourceFolder": "string",
+            "size": 0,
+            "packedSize": 0,
+            "creationTime": timestamp,
+            "type": "string",
+            "validationStatus": "string",
+            "keywords": [
+                "string"
+            ],
+            "description": "Reduction of " + input_dataset['datasetName'],
+            "datasetName": "Reduction no. " + self.random_pid(),
+            "classification": "string",
+            "license": input_dataset['license'],
+            "version": "string",
+            "isPublished": True,
+            "ownerGroup": "string",
+            "accessGroups": [
+                "string"
+            ],
+            "createdBy": "string",
+            "updatedBy": "string",
+            "createdAt": timestamp,
+            "updatedAt": timestamp,
+            "datasetlifecycle": {
+                "archivable": False,
+                "retrievable": True,
+                "publishable": True,
+                "dateOfDiskPurging": timestamp,
+                "archiveRetentionTime": timestamp,
+                "dateOfPublishing": timestamp,
+                "isOnCentralDisk": True,
+                "archiveStatusMessage": "string",
+                "retrieveStatusMessage": "string",
+                "archiveReturnMessage": {},
+                "retrieveReturnMessage": {},
+                "exportedTo": "string",
+                "retrieveIntegrityCheck": True
+            },
+            "history": [
+                {
+                    "id": "string"
+                }
+            ]
+        }
